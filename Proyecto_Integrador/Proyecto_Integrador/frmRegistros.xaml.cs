73bc6d63367561114246eb8d19082732c78f1fca
FcoMondro <fjgm11@hotmail.com>
2013-11-14 15:13:45 -0600

    Ni idea

--- Proyecto_Integrador/Proyecto_Integrador/frmRegistros.xaml.cs
+++ Proyecto_Integrador/Proyecto_Integrador/frmRegistros.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
 using System.Data.OleDb;
+using System.Data;
 
 namespace Proyecto_Integrador
 {
@@ -26,40 +27,64 @@ namespace Proyecto_Integrador
             InitializeComponent();
         }
 
+        private string Check_Sexo()
+        {
+            if (rdbMasculino.IsChecked == true)
+                return Convert.ToString(rdbMasculino.Content);
+            if (rdbFemenino.IsChecked == true)
+                return Convert.ToString(rdbFemenino.Content);
+            else
+                return "";
+        }
+
         private void btnAgregar_Click(object sender, RoutedEventArgs e)
         {
             OleDbConnection Conexion = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source='|DataDirectory|\BaseDeDatos.accdb'");
-            OleDbCommand Instruccion = new OleDbCommand("Select * From Usuario Where Id_Usuario=@Id_Usuario", Conexion);
-            //OleDbCommand Instruccion = new OleDbCommand("Insert Into Usuario (Id_Usuario,UserNombre,UserApPa,UserApMa,UserFechaNac,UserSexo,UserTipo,UserDireccionCalle,UserDireccionNumero,UserDireccionColonia,UserDireccionMunicipio,UserCelular,UserTelefono,UserTipoSangre,UserAlergia)" 
-            //VALUES (), Conexion);
-            //Instruccion.Parameters.AddWithValue("@Id_Usuario", txtID.Text);
-            //Instruccion.Parameters.AddWithValue("@UserNombre", txtNombres.Text);
-            //Instruccion.Parameters.AddWithValue("@UserApPa", Text,txtApellidoP.Text);
-            //Instruccion.Parameters.AddWithValue("@UserApMa", txtApellidoM.Text);
-            //Instruccion.Parameters.AddWithValue("@UserFechaNac", dtpFecha.DisplayDate);
-            //Instruccion.Parameters.AddWithValue("@UserSexo", Sexo());
-            //Instruccion.Parameters.AddWithValue("@UserTipo", cmbUsuarios.Text);
-            //Instruccion.Parameters.AddWithValue("@UserDireccionCalle", txtCalle.Text);
-            //Instruccion.Parameters.AddWithValue("@UserDireccionNumero", txtNumero.Text);
-            //Instruccion.Parameters.AddWithValue("@UserDireccionColonia", txtColonia.Text);
-            //Instruccion.Parameters.AddWithValue("@UserDireccionMunicipio", txtMunicipio.Text);
-            //Instruccion.Parameters.AddWithValue("@UserCelular", txtCelular.Text);
-            //Instruccion.Parameters.AddWithValue("@UserTelefono", txtCasa.Text);
-            //Instruccion.Parameters.AddWithValue("@UserTipoSangre", cmbSangre.Text);
-            //Instruccion.Parameters.AddWithValue("@UserAlergia", txtAlergias.Text);
-            //OleDbDataAdapter Agregar;
-            //Agregar = Instruccion.ExecuteNonQuery();
+            
+            OleDbCommand Instruccion= new OleDbCommand("Insert INTO Usuario (Id_Usuario,UserNombre,UserApPa,UserApMa,UserFechaNac,UserSexo,UserTipo,UserDireccionCalle,UserDireccionNumero,UserDireccionColonia,UserDireccionMunicipio,UserCelular,UserTelefono,UserTipoSangre,UserAlergias) VALUES (@Id_Usuario,@userNombre,@UserApPa,@UserApMa,@UserFechaNac,@UserSexo,@UserTipo,@UserDireccionCalle,@UserDireccionNumero,@UserDireccionColonia,@UserDireccionMunicipio,@UserCelular,@UserTelefono,@UserTipoSangre,@UserAlergia)", Conexion);
+            OleDbCommand Instruccion2= new OleDbCommand("Insert INTO UsuarioAccidente (Id_UserAccidente,UserAccidenteNombre,UserAccidenteCelular,UserAccidenteTelefono Values()",Conexion);
 
-            Instruccion.Parameters.AddWithValue("@Id_Usuario", 1);
-            OleDbDataReader Lector;
+            
+            Instruccion.Parameters.AddWithValue("@Id_Usuario", Int32.Parse(txtID.Text));
+            Instruccion.Parameters.AddWithValue("@UserNombre", txtNombres.Text);
+            Instruccion.Parameters.AddWithValue("@UserApPa", txtApellidoP.Text);
+            Instruccion.Parameters.AddWithValue("@UserApMa", txtApellidoM.Text);
+            Instruccion.Parameters.AddWithValue("@UserFechaNac", dtpFecha.DisplayDate);
+            Instruccion.Parameters.AddWithValue("@UserSexo", Check_Sexo());
+            Instruccion.Parameters.AddWithValue("@UserTipo", cmbUsuarios.Text);
+            Instruccion.Parameters.AddWithValue("@UserDireccionCalle", txtCalle.Text);
+            Instruccion.Parameters.AddWithValue("@UserDireccionNumero", Int32.Parse(txtNumero.Text));
+            Instruccion.Parameters.AddWithValue("@UserDireccionColonia", txtColonia.Text);
+            Instruccion.Parameters.AddWithValue("@UserDireccionMunicipio", txtMunicipio.Text);
+            Instruccion.Parameters.AddWithValue("@UserCelular", Int32.Parse(txtCelular.Text));
+            Instruccion.Parameters.AddWithValue("@UserTelefono", Int32.Parse(txtCasa.Text));
+            Instruccion.Parameters.AddWithValue("@UserTipoSangre", cmbSangre.Text);
+            Instruccion.Parameters.AddWithValue("@UserAlergia", txtAlergias.Text);
+
+            //Instruccion2.Parameters.AddWithValue("@Id_Usuario", txtID.Text);
+            //Instruccion2.Parameters.AddWithValue("@UserAccidenteNombre", txtNombreAccidente.Text);
+            //Instruccion2.Parameters.AddWithValue("@UserAccidenteCelular", txtMovilAccidente.Text);
+            //Instruccion2.Parameters.AddWithValue("@UserAccidenteCasa", txtCasaAccidente.Text);
+
+            //Instruccion.Parameters.AddWithValue("@Id_Usuario", 1);
+            //OleDbDataReader Lector;
+            
             Conexion.Open();
-            Lector = Instruccion.ExecuteReader();
+            Instruccion.ExecuteNonQuery();
+            //OleDbDataReader AgregarAccidente;
+            //AgregarUsuario.Read();
 
-            Lector.Read();
-            txtID.Text = Lector["Id_Usuario"].ToString();
-            txtNombres.Text = Lector["UserNombre"].ToString();
-            txtApellidoP.Text = Lector["UserApPa"].ToString();
-            txtApellidoM.Text = Lector["UserApMa"].ToString();
+            //AgregarUsuario = Instruccion.ExecuteNonQuery();
+            //AgregarAccidente = Instruccion2.ExecuteNonQuery();
+            //Lector = Instruccion.ExecuteReader();
+
+            //AgregarUsuario.Close();
+            //AgregarAccidente.Close();
+            //Lector.Read();
+            //txtID.Text = Lector["Id_Usuario"].ToString();
+            //txtNombres.Text = Lector["UserNombre"].ToString();
+            //txtApellidoP.Text = Lector["UserApPa"].ToString();
+            //txtApellidoM.Text = Lector["UserApMa"].ToString();
 
             Conexion.Close();
             
