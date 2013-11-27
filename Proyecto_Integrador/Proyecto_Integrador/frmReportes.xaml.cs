054732eeb88de29af38ad41d14490c48103fb5a7
FcoMondro <fjgm11@hotmail.com>
2013-11-27 13:44:16 -0600

    Frecuencia de uso(usuario)
    Ya funciona y busca

--- Proyecto_Integrador/Proyecto_Integrador/frmReportes.xaml.cs
+++ Proyecto_Integrador/Proyecto_Integrador/frmReportes.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Shapes;
 using MahApps.Metro.Controls;
 using System.Data.OleDb;
 using System.Data;
+using System.ComponentModel;
 
 namespace Proyecto_Integrador
 {
@@ -29,17 +30,22 @@ namespace Proyecto_Integrador
             cmd = new OleDbCommand("Select * FROM Servicios", Conexion);
             da = new OleDbDataAdapter(cmd);
             tabla = new DataTable();
+            dtpFinal.IsEnabled = false;
+
         }
+
         OleDbConnection Conexion;
         OleDbCommand cmd;
         OleDbDataAdapter da;
         DataTable tabla;
         List<String> numeroCamiones;
 
+        //Cobros
         public struct Uni
         {
             public string unidad;
-            public string Unidad {
+            public string Unidad
+            {
                 get { return unidad; }
             }
             public List<string> TiposUsuarios;
@@ -47,6 +53,7 @@ namespace Proyecto_Integrador
             {
                 return unidad;
             }
+
 
             public decimal Total
             {
@@ -65,11 +72,15 @@ namespace Proyecto_Integrador
                     return total;
                 }
             }
-            public decimal General {
-                get {
-                    decimal total=0;
-                    foreach(string v in TiposUsuarios){
-                        if (v == "General") {
+            public decimal General
+            {
+                get
+                {
+                    decimal total = 0;
+                    foreach (string v in TiposUsuarios)
+                    {
+                        if (v == "General")
+                        {
                             total += 6;
                         }
                     }
@@ -122,13 +133,16 @@ namespace Proyecto_Integrador
                 }
             }
         }
-        List<Uni> Unidad = new List<Uni>();
 
-        public void llenarStruct(ref Uni input)
+        BindingList<Uni> Unidad = new BindingList<Uni>();
+
+        public void llenarStruct(ref Uni input, DateTime fechaI, DateTime fechaF)
         {
             input.TiposUsuarios = new List<string>();
-            OleDbCommand cmd1 = new OleDbCommand("Select TipoUsuario FROM Servicios WHERE NumUnidad = @NumUnidad", Conexion);
+            OleDbCommand cmd1 = new OleDbCommand("Select TipoUsuario FROM Servicios WHERE NumUnidad = @NumUnidad AND Fecha BETWEEN @FechaI AND @FechaF", Conexion);
             cmd1.Parameters.AddWithValue("@NumUnidad", input.unidad);
+            cmd1.Parameters.AddWithValue("@FechaI", fechaI);
+            cmd1.Parameters.AddWithValue("@FechaF", fechaF);
             OleDbDataAdapter da1 = new OleDbDataAdapter(cmd1);
             DataTable tabla1 = new DataTable();
             da1.Fill(tabla1);
@@ -140,19 +154,38 @@ namespace Proyecto_Integrador
 
         public void boton()
         {
+            DateTime fechaInicial = DateTime.Now.Date;
+            DateTime fechaFinal = DateTime.Now.Date;
+            if (dtpInicial.SelectedDate.HasValue)
+                fechaInicial = dtpInicial.SelectedDate.Value;
+            else
+                MessageBox.Show("Seleccione una fecha inicial valida");
+            if (chkActual.IsChecked == true)
+            {
+                if (dtpFinal.SelectedDate.HasValue)
+                    fechaFinal = dtpFinal.SelectedDate.Value;
+                else
+                    MessageBox.Show("Seleccione una fecha dinal valida");
+            }
+            else
+                fechaFinal = DateTime.Now.Date;
+
             foreach (string str in numeroCamiones)
             {
                 Uni var = new Uni();
                 var.unidad = str;
-                llenarStruct(ref var);
+                llenarStruct(ref var, fechaInicial, fechaFinal);
                 Unidad.Add(var);
             }
         }
 
         private void cmdCargar_Click(object sender, RoutedEventArgs e)
         {
+
+            Unidad.Clear();
             boton();
             dtgCobros.ItemsSource = Unidad;
+
         }
 
         private void MetroWindow_Loaded_1(object sender, RoutedEventArgs e)
@@ -169,6 +202,72 @@ namespace Proyecto_Integrador
             numeroCamiones = (from DataRow var in dt.Rows select var.ItemArray[0].ToString()).ToList();
             Conexion.Close();
 
+
+        }
+
+        private void chkActual_Checked(object sender, RoutedEventArgs e)
+        {
+            dtpFinal.IsEnabled = true;
+        }
+
+        //Frecuencias de uso Usuario
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Usuario.Clear();
+            botonFrec();
+            dtgUsosUsuario.ItemsSource = Usuario;
+        }
+        
+        public struct FreUsuarios
+        {
+            public DataRow tabla;
+            public string NumUnidad
+            {
+                get { return tabla.ItemArray[1].ToString(); }
+            }
+            public DateTime Fecha
+            {
+                get { return (DateTime)tabla.ItemArray[4]; }
+            }
+
+        }
+        BindingList<FreUsuarios> Usuario = new BindingList<FreUsuarios>();
+
+        public void botonFrec()
+        {
+            string tarjeta = txtNumTarjeta.Text;
+            DateTime fechaInicial = DateTime.Now.Date;
+            DateTime fechaFinal = DateTime.Now.Date;
+            if (dtpInicialFUs.SelectedDate.HasValue)
+                fechaInicial = dtpInicialFUs.SelectedDate.Value;
+
+
+            if (dtpFinalUs.SelectedDate.HasValue)
+                fechaFinal = dtpFinalUs.SelectedDate.Value;
+
+
+            OleDbCommand cmdFUs = new OleDbCommand("Select * FROM Servicios WHERE Usuario = @Usuario AND Fecha BETWEEN @FechaI AND @FechaF ", Conexion);
+            cmdFUs.Parameters.AddWithValue("@Usuario", txtNumTarjeta.Text);
+            cmdFUs.Parameters.AddWithValue("@FechaI", fechaInicial);
+            cmdFUs.Parameters.AddWithValue("@FechaF", fechaFinal);
+            OleDbDataAdapter FrecenciaUsuario = new OleDbDataAdapter(cmdFUs);
+            DataTable FrecUsuario = new DataTable();
+            FrecenciaUsuario.Fill(FrecUsuario);
+            FreUsuarios tmp ;
+            int i = 0;
+            foreach (DataRow r in FrecUsuario.Rows) {
+                MessageBox.Show(Convert.ToString(i++));
+                tmp = new FreUsuarios();
+                tmp.tabla = r;
+                Usuario.Add(tmp);
+            }            
+        }
+
+        //Frecuencia de uso por Unidad
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
 
         }
     }
